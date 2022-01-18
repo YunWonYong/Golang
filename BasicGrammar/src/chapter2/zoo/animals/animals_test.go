b031d31654c9bc227c0912d2459e82af09b0c718
ywyi1992 <ywyi1992@gmail.com>
2022-01-18 22:51:46 +0900

    capter2 예제 코드

---
+++ BasicGrammar/src/chapter2/zoo/animals/animals_test.go
@@ -0,0 +1,27 @@
+package animals
+
+import "testing"
+
+func TestElephantFeed(t *testing.T) {
+	expect := "Grass"
+	actual := ElephantFeed()
+	if expect != actual {
+		t.Errorf("expect: %s, actual: %s", expect, actual)
+	}
+}
+
+func TestMonkeyFeed(t *testing.T) {
+	expect := "Banana"
+	actual := MonkeyFeed()
+	if expect != actual {
+		t.Errorf("expect: %s, actual: %s", expect, actual)
+	}
+}
+
+func TestRabbitFeed(t *testing.T) {
+	expect := "Carrot"
+	actual := RabbitFeed()
+	if expect != actual {
+		t.Errorf("expect: %s, actual: %s", expect, actual)
+	}
+}
---
+++ BasicGrammar/src/chapter2/zoo/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	"./animals"
+)
+
+func main() {
+	fmt.Println(AppName())
+	fmt.Println(animals.ElephantFeed())
+	fmt.Println(animals.MonkeyFeed())
+	fmt.Println(animals.RabbitFeed())
+}
