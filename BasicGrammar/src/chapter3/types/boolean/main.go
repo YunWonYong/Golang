259d3677df9af7f2dfb03eba164d5770cbe10a4b
ywyi1992 <ywyi1992@gmail.com>
2022-01-23 13:28:54 +0900

    chapter3 types - boolean

---
+++ BasicGrammar/src/chapter3/types/boolean/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	var expliciteBooleanVariable bool = true
+	impliciteBooleanVariable := false
+	var defaultBooleanVariable bool
+	fmt.Println(expliciteBooleanVariable, impliciteBooleanVariable, defaultBooleanVariable)
+}
