7df4a92005ad102f7dfa9ce0151910c4e71647a6
ywyi1992 <ywyi1992@gmail.com>
2022-01-23 13:28:26 +0900

    capter2 예제 코드 주석 추가

--- BasicGrammar/src/chapter2/zoo/main.go
+++ BasicGrammar/src/chapter2/zoo/main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	fmt.Println(AppName())
+	fmt.Println(AppName()) //go run main.go app.go 해야 실행됨
 	fmt.Println(animals.ElephantFeed())
 	fmt.Println(animals.MonkeyFeed())
 	fmt.Println(animals.RabbitFeed())
