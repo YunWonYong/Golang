b5a9aadc3d6d40b6708f217c9b1554e21dd34623
ywyi1992 <ywyi1992@gmail.com>
2022-01-24 12:35:38 +0900

    chapter3 types - signed Integer * wrap around test append

--- BasicGrammar/src/chapter3/types/integers/signeds/test/signedInteger.go
+++ BasicGrammar/src/chapter3/types/integers/signeds/test/signedInteger.go
@@ -19,3 +19,13 @@ func IntOrInt64() string {
 	i := math.MaxInt64
 	return reflect.TypeOf(i).Name()
 }
+
+func IntAround() (result int8) {
+	result = math.MaxInt8
+	result++
+	return
+}
+
+func IntAroundLoop(sumValue int8) int8 {
+	return math.MaxInt8 + sumValue
+}
--- BasicGrammar/src/chapter3/types/integers/signeds/test/signedInteger_test.go
+++ BasicGrammar/src/chapter3/types/integers/signeds/test/signedInteger_test.go
@@ -1,6 +1,7 @@
 package signedInteger
 
 import (
+	"math"
 	"testing"
 )
 
@@ -19,5 +20,28 @@ func TestIntOfInt64(t *testing.T) {
 func TestIntOrInt64(t *testing.T) {
 	if IntOrInt64() != "int" {
 		t.Error("meth.MaxInt64의 타입은 int임")
+	}
+}
+
+func TestIntAround(t *testing.T) {
+	actual := IntAround()
+	if actual != math.MinInt8 {
+		t.Errorf("actual: %d, type: %T", actual, actual)
+	}
+}
+
+func TestIntAroundLoop(t *testing.T) {
+	var (
+		except, actual, index, length int8 = math.MinInt8, 0, 0, math.MaxInt8
+	)
+
+	for index < length {
+		index++
+		actual = IntAroundLoop(index)
+		if except != actual {
+			t.Errorf("actual: %d, except: %d", actual, except)
+		}
+
+		except++
 	}
 }
