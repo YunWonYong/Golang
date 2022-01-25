f0aa4b594c8c1624706be188a345fb0f88e3a57c
ywyi1992 <ywyi1992@gmail.com>
2022-01-25 12:16:26 +0900

    chapter3 types - signed Integer * wrap around test move => overflow directory

--- BasicGrammar/src/chapter3/types/integers/signeds/test/signedInteger_test.go
+++ BasicGrammar/src/chapter3/types/integers/signeds/test/signedInteger_test.go
@@ -22,26 +22,3 @@ func TestIntOrInt64(t *testing.T) {
 		t.Error("meth.MaxInt64의 타입은 int임")
 	}
 }
-
-func TestIntAround(t *testing.T) {
-	actual := IntAround()
-	if actual != math.MinInt8 {
-		t.Errorf("actual: %d, type: %T", actual, actual)
-	}
-}
-
-func TestIntAroundLoop(t *testing.T) {
-	var (
-		except, actual, index, length int8 = math.MinInt8, 0, 0, math.MaxInt8
-	)
-
-	for index < length {
-		index++
-		actual = IntAroundLoop(index)
-		if except != actual {
-			t.Errorf("actual: %d, except: %d", actual, except)
-		}
-
-		except++
-	}
-}
