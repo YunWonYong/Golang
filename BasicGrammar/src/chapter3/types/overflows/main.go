97d6fc73989ef283324dd39a2356ba0d2cccc2a3
ywyi1992 <ywyi1992@gmail.com>
2022-02-01 14:52:31 +0900

    chapter3 types - overflow add

--- BasicGrammar/src/chapter3/types/overflows/main.go
+++ BasicGrammar/src/chapter3/types/overflows/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math"
+	"strconv"
 )
 
 func main() {
@@ -10,7 +11,9 @@ func main() {
 }
 
 func OverflowCasting() {
-	var a int16 = math.MaxInt8 + 1
-	var b int8 = int8(a)
-	fmt.Printf("a: %d, b: %d\n", a, b)
+	var a int16 = math.MaxInt8
+	fmt.Printf("in8Max: %d, binaryCode: %08s\n", a, strconv.FormatInt(int64(a), 2))
+	fmt.Printf("in8Max + 1: %d, binaryCode: %09s\n", a, strconv.FormatInt(int64(a + 1), 2))
+	var b int8 = int8(a + 1)
+	fmt.Printf("wrap around: %d, b: %09s\n", b, strconv.FormatInt(int64(b), 2))
 }
