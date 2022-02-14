d051910e52f0f3146d5e97a8deae73ff7945fcdc
ywyi1992 <ywyi1992@gmail.com>
2022-02-14 21:40:16 +0900

    chapter3 scope - file add go run main.go app.go

--- BasicGrammar/src/chapter3/scopes/file/main.go
+++ BasicGrammar/src/chapter3/scopes/file/main.go
@@ -6,6 +6,6 @@ import (
 
 func main() {
 	fmt.Println("망할 IDE 왜 자꾸 에러로 표시하는거야 ㅂㄷㅂㄷ")
-	//fmt.Println("AppVersion:", AppVersion)
-	//printMessage("Hello!")
+	fmt.Println("AppVersion:", AppVersion)
+	printMessage("Hello!")
 }
