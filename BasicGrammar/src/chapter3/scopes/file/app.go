6bf7764c79182cd7612d6feaeaddbf1aaf9d66af
ywyi1992 <ywyi1992@gmail.com>
2022-02-14 21:33:00 +0900

    chapter3 scope - file add app.go 파일과 main.go 파일은 표기법 상관없이 자원을 공유할 수 있지만 import는 안됌

---
+++ BasicGrammar/src/chapter3/scopes/file/app.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	f "fmt"
+)
+
+const (
+	AppVersion = 1.0
+)
+
+func printMessage(s string) {
+	f.Println(s)
+}
---
+++ BasicGrammar/src/chapter3/scopes/file/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("망할 IDE 왜 자꾸 에러로 표시하는거야 ㅂㄷㅂㄷ")
+	//fmt.Println("AppVersion:", AppVersion)
+	//printMessage("Hello!")
+}
