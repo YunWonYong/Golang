63549b85c99140641b000095fae76054ff7c2922
ywyi1992 <ywyi1992@gmail.com>
2022-02-15 12:44:35 +0900

    chapter3 for statement -  add 1. baseForStatement 2. infinityLoopAndBreak 3. initStatementFor 4. conditionExpressionFor 5. postStatementAndContinueFor 6. postStatementAndContinueFor2

---
+++ BasicGrammar/src/chapter3/controlStatements/for/main.go
@@ -0,0 +1,64 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("=====baseForStatement=====")
+	baseForStatement()
+	fmt.Println("=====infinityLoopAndBreak=====")
+	infinityLoopAndBreak()
+	fmt.Println("=====initStatementFor=====")
+	initStatementFor()
+	fmt.Println("=====conditionExpressionFor=====")
+	conditionExpressionFor()
+	fmt.Println("=====postStatementAndContinueFor=====")
+	postStatementAndContinueFor()
+}
+
+func baseForStatement() {
+	defer fmt.Println()
+	for i := 1; i < 11; i++ {
+		fmt.Printf("%d ", i)
+	}
+}
+
+func infinityLoopAndBreak() {
+	breakPoint := 0
+	for {
+		if breakPoint == 100 {
+			fmt.Println("break!!!")
+			break
+		}
+		breakPoint++
+	}
+}
+
+func initStatementFor() {
+	for i := 0; ; {
+		if i == 2 {
+			fmt.Printf("i(%d) stop!\n", i)
+			break
+		}
+		i++
+	}
+}
+
+func conditionExpressionFor() {
+	i := 2
+	defer fmt.Println("defer i: ", i) // ???? 왜 2??
+	for i < 100 {
+		i++
+	}
+	fmt.Println("loop end i: ", i)
+}
+
+func postStatementAndContinueFor() {
+	defer fmt.Println()
+	i := 0
+	for ; i < 101; i++ {
+		if i%2 != 0 {
+			continue
+		}
+		fmt.Printf("%d ", i)
+	}
+}
