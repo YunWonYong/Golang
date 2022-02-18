1933bcae1e4d780ae51ff1d31a582cfd2c52f65a
ywyi1992 <ywyi1992@gmail.com>
2022-02-18 16:32:50 +0900

    chapter4 slice varags append array를 slice처럼 사용하려면 결국 slice로 변경해서 해야한다.

--- BasicGrammar/src/chapter4/slice/case5_varargs/main.go
+++ BasicGrammar/src/chapter4/slice/case5_varargs/main.go
@@ -6,9 +6,10 @@ func main() {
 	a := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
 	sumValue := sum(a...)
 	fmt.Printf("a %#v element sum: %d\n", a, sumValue)
-	a = append(a, 10)
-	sumValue = sum(a...)
-	fmt.Printf("a %#v element sum: %d\n", a, sumValue)
+	b := [10]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	// sumValue = sum(b...) // ...int는 []int 타입이다.
+	sumValue = sum(b[:]...)
+	fmt.Printf("b %#v element sum: %d\n", b, sumValue)
 }
 
 func sum(a ...int) (result int) {
