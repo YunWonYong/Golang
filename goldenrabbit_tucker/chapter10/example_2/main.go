6676749d6ed2c6978c7fc1025e066aba63efbeb9
wonyong.yun <ywyi1992@gmail.com>
2023-10-09 18:31:45 +0900

    chapter 10 switch statement example 2

---
+++ goldenrabbit_tucker/chapter10/example_2/main.go
@@ -0,0 +1,90 @@
+package main
+
+import "fmt"
+
+const (
+	PLUS string = "+"
+	MINUS string = "-"
+	MULTI  string = "*"
+	DIV string = "/"
+	REMAINDER string = "%"
+	SQUARE string = "**"
+)
+
+const (
+	PLUS_NO int = 1
+        MINUS_NO int = 2
+        MULTI_NO int = 3
+        DIV_NO int = 4
+        REMAINDER_NO int = 5
+        SQUARE_NO int = 6
+)
+
+func main() {
+        var (
+                leftOperand int64
+                rightOperand int64
+                operation int
+        )
+
+        fmt.Println("피연산자 두 개를 입력해 주세요.")
+        fmt.Printf("왼쪽 피연산자: ")
+        fmt.Scanf("%d", &leftOperand)
+        fmt.Printf("오른쪽 피연산자: ")
+        fmt.Scanf("%d", &rightOperand)
+
+        fmt.Println("연산자를 선택해주세요.")
+        fmt.Println("1. +, 2. -, 3. *, 4. /, 5. %, 6. **")
+        fmt.Scanf("%d", &operation)
+
+        opcode := getOpcode(operation)
+
+        result := fmt.Sprintf("%d번은 잘 못된 입력입니다.", operation)
+
+        if operation == SQUARE_NO {
+                result = fmt.Sprintf("%d(%s)번 연산자는 지원하고 있지 않습니다.", operation, opcode)
+        } else if (operation == DIV_NO || operation == REMAINDER_NO) && rightOperand == 0 {
+                result = fmt.Sprintf("%d %s %d는 연산할 수 없습니다.", leftOperand, opcode, rightOperand)
+        } else if operation > 0 && operation < 7 {
+                result = calc(leftOperand, rightOperand, opcode)
+        }
+
+        fmt.Println("결과: ", result)
+}
+
+func calc(left, right int64, opcode string) string {
+	var result int64
+	switch opcode {
+	case PLUS:
+		result = left + right
+	case MINUS:
+		result = left - right
+	case MULTI:
+		result = left * right
+	case DIV:
+		result = left / right
+	case REMAINDER:
+		result = left % right
+	case SQUARE:
+
+	}
+	return fmt.Sprintf("%d %s %d = %d", left, opcode, right, result)
+}
+
+func getOpcode(operation int) (opcode string) {
+	switch operation {
+	case PLUS_NO:
+                opcode = PLUS
+	case MINUS_NO:
+                opcode = MINUS
+	case MULTI_NO:
+                opcode = MULTI
+	case DIV_NO:
+                opcode = DIV
+	case REMAINDER_NO:
+                opcode = REMAINDER
+	case SQUARE_NO:
+		opcode = SQUARE
+	}
+        return
+}
