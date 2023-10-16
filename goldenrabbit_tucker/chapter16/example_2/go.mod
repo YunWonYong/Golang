759811fecf16f849ce47dd6cfa148502803473ea
wonyong.yun <ywyi1992@gmail.com>
2023-10-16 16:02:59 +0900

    chapter 16 import example 2

---
+++ goldenrabbit_tucker/chapter16/example_2/go.mod
@@ -0,0 +1,3 @@
+module gh/chap16/ex2/ex
+
+go 1.18
---
+++ goldenrabbit_tucker/chapter16/example_2/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	fmt.Println(strings.Join([]string{
+		"hello",
+		" world",
+		"!!!",
+	}, ""))
+}
