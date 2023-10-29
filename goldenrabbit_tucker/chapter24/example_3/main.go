58fa0cbdb7c911c93874ab830f98cbfef7469d19
wonyong.yun <ywyi1992@gmail.com>
2023-10-29 16:38:22 +0900

    chapter 24 goroutine example 3-2

--- goldenrabbit_tucker/chapter24/example_3/main.go
+++ goldenrabbit_tucker/chapter24/example_3/main.go
@@ -6,7 +6,10 @@ import (
 	"time"
 )
 
-var wg sync.WaitGroup
+var (
+	wg    sync.WaitGroup
+	mutex sync.Mutex
+)
 
 type Account struct {
 	Balance int
@@ -19,12 +22,14 @@ func main() {
 	for i := 0; i < routineCount; i++ {
 		go func() {
 			for {
+				mutex.Lock()
 				if account.Balance < 0 {
 					panic("잔고가 0보다 작습니다.")
 				}
 				account.Balance += 1000
 				time.Sleep(time.Millisecond)
 				account.Balance -= 1000
+				mutex.Unlock()
 			}
 			wg.Done()
 		}()
