0771653da5ab85d639316f61f72fbf174c4e82a4
wonyong.yun <ywyi1992@gmail.com>
2023-10-29 15:46:54 +0900

    chapter 24 goroutine example 2-5

--- goldenrabbit_tucker/chapter24/example_2/main.go
+++ goldenrabbit_tucker/chapter24/example_2/main.go
@@ -10,10 +10,10 @@ var wg sync.WaitGroup
 func main() {
 	for index, at := range "hello world!!!" {
 		wg.Add(1)
-		go func() {
+		go func(index int, at rune) {
 			fmt.Printf("%d번 서브루틴의 단어: %s\n", index, string(at))
 			wg.Done()
-		}()
+		}(index, at)
 	}
 	wg.Wait()
 }
